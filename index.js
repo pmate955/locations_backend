61659eb876799e7374b759c07114441b7d1c23c0
Mate Pinter <pmate955@gmail.com>
2020-01-17 17:50:25 +0100

    added dockerfile, and some scripts for db

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('ASD');
-})
+  res.send('ASD');
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+;
