d46d3f3dc9fe42d41f7b738d7f266b01abd04f29
Mate Pinter <pmate955@gmail.com>
2020-07-04 14:43:39 +0200

    fixed login

--- src/app/controllers/session.ts
+++ src/app/controllers/session.ts
@@ -7,7 +7,7 @@ import * as bcrypt from 'bcrypt';
 export const create = async (req: Request, res: Response) => {
   try {
     const user: User = await database('users').select().where({
-      username: req.body.username
+      email: req.body.email
     }).first();
     if (typeof user !== 'undefined' && bcrypt.compareSync(req.body.password, user.passwordHash)) {
       const info = { userId: user.id };
