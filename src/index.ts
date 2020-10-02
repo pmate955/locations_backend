6d073fb5fdef633597c0c7a29b0b22c83c2f80a7
Mate Pinter <pmate955@gmail.com>
2020-10-02 21:49:39 +0200

    added socket for room communictaion

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import * as cors from 'cors';
 import { Application } from 'express';
 import { router } from './app/routers';
 import { authentication }from './lib/auth';
+import { initSocket } from './lib/socket';
 
 const app: Application = express();
 app.use(cors());
@@ -11,20 +12,8 @@ app.use(authentication);
 app.use(router);
 const { PORT = 3000 } = process.env;
 let http = require("http").Server(app);
-const socket = require('socket.io')(http, {serveClient: false});
+initSocket(http);
 
-socket.on('connection', (socket) => {
-  console.log('Connected')
-});
-
-socket.on('new-message', (message) => {
-  console.log('MESSAGE', message)
-  socket.emit(message);
-});
-
-socket.on('error', function (err) {
-  console.log(err);
-});
 
 http.listen(PORT, () => {
   console.log(`server started at http://localhost:${PORT}`);
---
+++ src/lib/socket.ts
@@ -0,0 +1,43 @@
+import * as jwt from "jsonwebtoken";
+import User from "../app/models/user";
+import { database } from "./database";
+
+export const initSocket = (http) => {
+
+  const io = require('socket.io')(http, {serveClient: false});
+  const messages = [];
+  
+  io.use(async (socket, next) => {
+    try {
+      const token: string = socket.handshake.query.token;
+      const info = jwt.verify(token, process.env.JWT_SECRET);
+      const userId: number =  info.userId;
+      const user: User = await database('users').where({ id: userId }).first();
+      if(!user) {
+        next(new Error('Authentication error'));
+      }
+      socket.decodedUser = user;
+      next();
+    } catch(err) {
+      next(new Error('Authentication error'));
+    }    
+  }).on('connection', (socket) => {
+    
+    socket.join('common room');
+
+    socket.on('new-message', (message) => {
+      const dbMessage = {
+        creatorId: socket.decodedUser.id,
+        creatorName: socket.decodedUser.username,
+        body: message.body
+      }
+      messages.push(dbMessage);
+      io.in('common room').emit('new-message', dbMessage);
+    });
+    
+    socket.on('error', function (err) {
+      console.log(err);
+    });
+    
+  });
+}
